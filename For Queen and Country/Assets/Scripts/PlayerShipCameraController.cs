9a9a7b017e4097a517c4cf97af0296e0e437db11
JP9667 <jp@jpbackman.com>
2018-03-19 15:32:50 -0500

    Level Demo changes
    The following changes have been made:
    - The basic level layout for the demo has been finished and within Scene02
    - Prefabs have been added such as cannons, cannon ball, enemy ship, and treasure ship
    - Previous Ship movement has been scrapped and updated to allow the player to maneuver the ship better
    - Player ship can now fire cannon balls from the sides of his ship
    - Player ship camera is updated to allow the player to rotate the camera using their mouse
    - And other minor changes, etc.

---
+++ For Queen and Country/Assets/Scripts/PlayerShipCameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipCameraController : MonoBehaviour {
+
+    public float mouseSensitivity = 4.0f;
+    public float rotationSmoothTime = 8;
+
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    private Vector3 currentRotation;
+    private float yaw;
+    private float pitch;
+
+    // Use this for initialization
+    //void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        currentRotation = Vector3.Lerp(currentRotation, new Vector3(pitch, yaw), rotationSmoothTime * Time.deltaTime);
+
+        transform.eulerAngles = currentRotation;
+    }
+}
--- For Queen and Country/Assets/Scripts/PlayerShipController.cs
+++ For Queen and Country/Assets/Scripts/PlayerShipController.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 
 public class PlayerShipController : MonoBehaviour {
 
-    public float speed = 2f;
+    public float speed = 0.075f;
+    public float steerSpeed = 0.75f;
+    public float movementThreshold;
+    public float cannonballSpeed = 20.0f;
 
     //public bool inWindZone = false;
-    private Rigidbody rb;
+    //private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
+    private float movementFactor;
+    private float steerFactor;
+
+    public GameObject cannonball;
+    public GameObject[] portCannons = new GameObject[3];
+    private GameObject[] portCannonballs = new GameObject[3];
+    public GameObject[] starBoardCannons = new GameObject[3];
+    private GameObject[] starBoardCannonballs = new GameObject[3];
+
+    // Use this for initialization
+    void Start () {
+        //rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float hAxis = Input.GetAxis("Horizontal");
+        /*float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
 
         if(vAxis < 0)
@@ -27,13 +39,44 @@ public class PlayerShipController : MonoBehaviour {
 
         Vector3 movement = new Vector3(-vAxis, 0, hAxis) * speed * Time.deltaTime;
 
-        rb.MovePosition(transform.position + movement);
+        rb.MovePosition(transform.position + movement);*/
+
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        movementFactor = Mathf.Lerp(movementFactor, vAxis, Time.deltaTime / movementThreshold);
+
+        transform.Translate(-(movementFactor) * speed, 0.0f, 0.0f);
+
+        steerFactor = Mathf.Lerp(steerFactor, hAxis * vAxis, Time.deltaTime / movementThreshold);
+
+        transform.Rotate(0.0f, steerFactor * steerSpeed, 0.0f);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                //Vector3 cannonExit = new Vector3(portCannons[i].transform.position.x, portCannons[i].transform.position.y, portCannons[i].transform.position.z - 0.1f);
+                portCannonballs[i] = Instantiate(cannonball, portCannons[i].transform.position, portCannons[i].transform.rotation);
+                portCannonballs[i].GetComponent<Rigidbody>().velocity = portCannonballs[i].transform.forward * cannonballSpeed;
+                Destroy(portCannonballs[i], 1.0f);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                starBoardCannonballs[i] = Instantiate(cannonball, starBoardCannons[i].transform.position, starBoardCannons[i].transform.rotation);
+                starBoardCannonballs[i].GetComponent<Rigidbody>().velocity = starBoardCannonballs[i].transform.forward * cannonballSpeed;
+                Destroy(starBoardCannonballs[i], 1.0f);
+            }
+        }
     }
 
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
         rb.AddRelativeForce(Vector3.left * 2 - rb.velocity);
-    }
+    }*/
 
     private void OnCollisionEnter(Collision collision)
     {
