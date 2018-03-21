effc2a81e92b6370574ff64d055514fe86db8690
Hbart <hbartm21@gmail.com>
2018-03-20 19:29:47 -0500

    Add files via upload
    assets with whale ai

--- For Queen and Country/Assets/Scripts/whaleControl.cs
+++ For Queen and Country/Assets/Scripts/whaleControl.cs
@@ -1,17 +1,54 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//sing UnityEngine.AI;
 
-public class whaleControl : MonoBehaviour
-{
+public class whaleControl : MonoBehaviour {
+    public Transform[] Waypoints;
+    public float Speed;
+    public int curWayPoint;
+    public bool doPatrol = true;
+    public Vector3 Target;
+    public Vector3 MoveDirection;
+    public Vector3 Velocity;
+    private Rigidbody whale;
 
-    public int speed;
-    public GameObject player;
-
-    void Update()
+    private void Start()
     {
-        Vector3 localPosition = player.transform.position - transform.position;
-        localPosition = localPosition.normalized; // The normalized direction in LOCAL space
-        transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+        whale = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(curWayPoint < Waypoints.Length)
+        {
+            Target = Waypoints[curWayPoint].position;
+            MoveDirection = Target - transform.position;
+            //whale.transform.Rotate(0, -1, 0);
+            Velocity = whale.velocity;
+         
+            if(MoveDirection.magnitude < 1)
+            {
+                curWayPoint++;
+            }
+            else
+            {
+                Velocity = MoveDirection.normalized * Speed;
+            }
+        }
+        else
+        {
+            if(doPatrol)
+            {
+                curWayPoint = 0;
+            }
+            else
+            {
+                Velocity = Vector3.zero;
+            }
+        }
+        whale.velocity = Velocity;
+        transform.LookAt(Target);
     }
 }
 
